edac7e056b37a8ab1bd6ce23918be7b4a41c7675
Eduardo Augusto Almeida lira <eduz92337@gmail.com>
2025-06-24 18:14:20 -0300

    Update image_reconstruction_genetic_algorithm.py

--- image_reconstruction_genetic_algorithm.py
+++ image_reconstruction_genetic_algorithm.py
@@ -5,21 +5,18 @@ import pygame,random,PIL
 
 
 WIDTH,HEIGHT = 800,800
-img = PIL.Image.open('/home/eduardo/Estudos/Projetos/algoritmos_geneticos/paisagem.jpg.webp').convert('RGB').resize((WIDTH, HEIGHT))
-CELL_SIZE = 10
+img = PIL.Image.open('einstein.png').convert('RGB').resize((WIDTH, HEIGHT))
+CELL_SIZE = 15
 GRID_HEIGHT = HEIGHT//CELL_SIZE
 GRID_WIDTH = WIDTH//CELL_SIZE
 
 
 class celula():
-    def __init__(self, init_random=True):
-        if init_random:
-            self.color = [random.randint(0, 255) for _ in range(3)]
-        else:
-            self.color = [0, 0, 0] 
+    def __init__(self,):
+       self.color = [random.randint(0,255) for _ in range(3)]
 class Cromossomo():
-    def __init__(self, init_random=True):
-        self.genoma = [[celula(init_random) for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+    def __init__(self, ):
+        self.genoma = [[celula() for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
     
     def show_cromossomo(self, screen):
         for i in range(GRID_HEIGHT):
@@ -42,6 +39,7 @@ for i in range(GRID_HEIGHT):
         linha.append([r_alvo//count, g_alvo//count, b_alvo//count])
     target_colors.append(linha)    
 
+
 class GeneticAlgo():
     def __init__(self,number_generations):
         self.population = [Cromossomo() for _ in range(number_generations)]
@@ -58,23 +56,13 @@ class GeneticAlgo():
 
     
     def selection(self):
-        
-        fitnesses = [self.fitness(ind) for ind in self.population]
-        total_fitness = sum(fitnesses)
-        
-        if total_fitness == 0:
-            return random.sample(self.population, len(self.population)//2)
-        
-        
         pais = []
         for _ in range(len(self.population)//2):
-            pick = random.uniform(0, total_fitness)
-            current = 0
-            for i, ind in enumerate(self.population):
-                current += fitnesses[i]
-                if current > pick:
-                    pais.append(ind)
-                    break
+            n = min(3, len(self.population))
+            candidatos = random.sample(self.population, n)
+
+            melhor = max(candidatos, key=self.fitness)
+            pais.append(melhor)
         return pais
     
     def mutation(self):
@@ -95,7 +83,7 @@ class GeneticAlgo():
         filhos = []
         for i in range(0, len(pais)-1, 2):
             pai1, pai2 = pais[i], pais[i+1]
-            filho = Cromossomo(init_random=False)
+            filho = Cromossomo()
             
             for x in range(GRID_HEIGHT):
                 for y in range(GRID_WIDTH):
@@ -123,11 +111,6 @@ class GeneticAlgo():
         self.population[0].show_cromossomo(screen)
         
         
-
-
-
-        
-
 
 def main():
     pygame.init()
